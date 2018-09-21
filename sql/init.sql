c71ff990fbeaabe7c915a0a8c8c5bfc6a5cc71c5
115design <115.design.studio@gmail.com>
2018-09-21 21:34:02 +0900

    Update init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS mysite CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE USER IF NOT EXISTS 'mysiteuser'@'%' IDENTIFIED BY 'mysitepass';
-GRANT ALL PRIVILEGES ON mysite.* TO 'mysiteuser'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'mysiteuser'@'%';
 
 FLUSH PRIVILEGES;
