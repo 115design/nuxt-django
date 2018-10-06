2599a3d22a6532fee88fe9b67c40518d0f5acb72
115design <115.design.studio@gmail.com>
2018-10-06 22:11:18 +0900

    Update nuxt.config.js

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -21,19 +21,7 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
+
   }
 }
 
